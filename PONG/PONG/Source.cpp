b58d3ac1907434d946ac9c8c79b2e66388a5a9d8
Spenxo <656038@dpsk12.net>
2017-05-19 08:54:04 -0600

    PONG

--- PONG/PONG/Source.cpp
+++ PONG/PONG/Source.cpp
@@ -2,9 +2,15 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_font.h>
+#include <windows.h>
+#include <stdio.h>
+#include <allegro5\allegro_audio.h>
+#include <allegro5\allegro_acodec.h>
+#include <allegro5/allegro_image.h>
+using namespace std;
 
 
-int collision(int b1x, int b1y, int b1w, int b1h, int b2x, int b2y, int b2w, int b2h);
+int collision(int b1x, int b1y, int b1w, int b1h, int b2x, int b2y, int b2w, int b2h, int bouncer_dx, int bouncer_dy);
 int main() {
 	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
@@ -12,14 +18,20 @@ int main() {
 	ALLEGRO_BITMAP *square = NULL;
 	ALLEGRO_BITMAP *square2 = NULL;
 	ALLEGRO_BITMAP *bouncer = NULL;
+	ALLEGRO_BITMAP *midwall = NULL;
 	ALLEGRO_FONT *font = NULL;
+	cout << "flag1" << endl;
+	ALLEGRO_SAMPLE *sample3 = NULL;
+	ALLEGRO_SAMPLE_INSTANCE *instance3 = NULL;
+	ALLEGRO_BITMAP *ball = NULL;
+	ALLEGRO_BITMAP *BGpic = NULL; 
 
 	//these two variables hold the x and y positions of the square
 	//initalize these variables to where you want your square to start
-	float square_x = 40;
+	float square_x = 0;
 	float square_y = 40;
 
-	float square2_x = 570;
+	float square2_x = 610;
 	float square2_y = 40;
 
 	float bouncer_x = 480/2;
@@ -28,12 +40,12 @@ int main() {
 	int squarescore = 0;
 	int square2score = 0;
 
-
+	cout << "flag2" << endl;
 	//here's our key states. they're all starting as "false" because nothing has been pressed yet.
 	//the first slot represents "up", then "down", "left" and "right"
 	bool key2[4] = { false, false, false, false };
 	bool key[4] = { false, false, false, false };
-	float bouncer_dx = -5.0, bouncer_dy = 5.0;
+	float bouncer_dx = -10, bouncer_dy = 10;
 	
 	//don't redraw until an event happens
 	bool redraw = true;
@@ -42,22 +54,31 @@ int main() {
 	bool doexit = false;
 
 	al_init();
-
+	cout << "flag3" << endl;
 	//get the keyboard ready to use
 	al_install_keyboard();
-
-	al_init_ttf_addon();
 	al_init_font_addon();
-
+	al_init_ttf_addon();
+	
+	al_init_image_addon();
+	
+	al_install_audio();
+	al_init_acodec_addon();
+	
 	timer = al_create_timer(.02);
 
 	display = al_create_display(640, 480);
 
 	font = al_load_ttf_font("Pong.ttf", 100, 0);
-
+	cout << "flag4" << endl;
+	sample3 = al_load_sample("BGmusic.wav");
+    al_reserve_samples(10);
+	midwall = al_create_bitmap(30, 640);
 	square = al_create_bitmap(32, 128);
 	square2 = al_create_bitmap(32, 128);
 	bouncer = al_create_bitmap(42, 42);
+	ball = al_create_bitmap(42, 42);
+	BGpic = al_create_bitmap(640, 480);
 
 	al_set_target_bitmap(square);
 	al_clear_to_color(al_map_rgb(0, 0, 255));
@@ -68,10 +89,13 @@ int main() {
 	al_set_target_bitmap(bouncer);
 	al_clear_to_color(al_map_rgb(255, 255, 255));
 
+	al_set_target_bitmap(midwall);
+	al_clear_to_color(al_map_rgb(255, 255, 255));
+
 	al_set_target_bitmap(al_get_backbuffer(display));
 
 	event_queue = al_create_event_queue();
-
+	cout << "flag5" << endl;
 	//these lines tell teh event source what to look for
 	al_register_event_source(event_queue, al_get_display_event_source(display));
 
@@ -81,15 +105,19 @@ int main() {
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
 
 	al_clear_to_color(al_map_rgb(0, 0, 0));
+	instance3 = al_create_sample_instance(sample3);
+	al_set_sample_instance_playmode(instance3, ALLEGRO_PLAYMODE_LOOP);
+
+	al_attach_sample_instance_to_mixer(instance3, al_get_default_mixer());
 
 	al_flip_display();
 
 	al_start_timer(timer);
-
+	cout << "flag6" << endl;
 	//so the game loop is set to act on "ticks" of the timer OR keyboard presses 
 	//OR the mouse closing the display
-	while (!doexit)
-	{
+
+	while (!doexit) {
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
 
@@ -120,37 +148,50 @@ int main() {
 			//really important code!
 			//move the box in a diagonal
 			bouncer_x += bouncer_dx;
-			bouncer_y += bouncer_dy;
+			bouncer_y -= bouncer_dy;
 
 			//redraw at every tick of the timer
 			redraw = true;
 		}
-
+		cout << "flag7" << endl;
 		if (ev.type == ALLEGRO_EVENT_TIMER) {
 
 			if (key2[0] && square2_y >= 0) {
 				square2_y -= 8.0;
 			}
-			if (key[0] && square_y >= 0) {
-				square_y -= 8.0;
-			}
 
 			if (key2[1] && square2_y <= 480 - 128) {
 				square2_y += 8.0;
 			}
+
+			//square2_y = bouncer_y - 64;
+			
+			if (key[0] && square_y >= 0) {
+				square_y -= 8.0;
+			}
+
 			if (key[1] && square_y <= 480 - 128) {
 				square_y += 8.0;
 			}
+			cout << "flag8" << endl;
+			if (collision(bouncer_x, bouncer_y, 42, 42, square_x, square_y, 32, 128, bouncer_dx, bouncer_dy) == 1) {
+			bouncer_dx = -bouncer_dx;
+			PlaySound(TEXT("PongBounce.wav"), NULL, SND_FILENAME | SND_ASYNC);
+			
+		}
+			if (collision(bouncer_x, bouncer_y, 42, 42, square2_x, square2_y, 32, 128, bouncer_dx, bouncer_dy) == 1) {
+			bouncer_dx = -bouncer_dx;
+			PlaySound(TEXT("PongBounce.wav"), NULL, SND_FILENAME | SND_ASYNC); 
+		}
 		}
 
-		if (collision(bouncer_x, bouncer_y ,42,42, square_y, square2_x,32,128) == 1)
-			bouncer_dx = -bouncer_dx;
+		
 		
 		///////////////////////////////////////////////////////////////////////////////////////////////////////
 		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
 			break;
 		}
-
+		
 		//////////////////////////////////////////////////////////////////////////////////////////////////////
 		//here's the algorithm that turns key presses into events
 		//a "key down" event is when a key is pushed
@@ -181,6 +222,7 @@ int main() {
 				break;
 			}
 		}
+		
 		//has something been released on the keyboard?
 		else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
 			switch (ev.keyboard.keycode) {
@@ -206,7 +248,7 @@ int main() {
 				break;
 			}
 		}
-
+		cout << "flag9" << endl;
 
 		//RENDER SECTION
 		//if the clock ticked but no other events happened, don't bother redrawing
@@ -219,41 +261,58 @@ int main() {
 			//the algorithm above just changes the x and y coordinates
 			//here's where the bitmap is actually drawn somewhere else
 
+			//BGpic = al_load_bitmap("BackGround.jpg");
+
+			//al_draw_bitmap(BGpic, 0, 0, 0);
+
 			al_draw_bitmap(square, square_x, square_y, 0);
 
 			al_draw_bitmap(square2, square2_x, square2_y, 0);
 
-			al_draw_bitmap(bouncer, bouncer_x, bouncer_y, 0);
+			//al_draw_bitmap(bouncer, bouncer_x, bouncer_y, 0);
+
+			ball = al_load_bitmap("SmashBall.png");
+
+			al_draw_bitmap(ball, bouncer_x, bouncer_y, 0);
+
+			al_draw_bitmap(midwall, -50, 0, 0);
 
 			al_draw_textf(font, al_map_rgb(255, 255, 255), 69, 40, ALLEGRO_ALIGN_LEFT, "%i", squarescore);
 			al_draw_textf(font, al_map_rgb(255, 255, 255), 570, 40, ALLEGRO_ALIGN_RIGHT, "%i", square2score);
 
+			al_play_sample_instance(instance3);
+
 			al_flip_display();
 		}
 	}
+	cout << "flag10" << endl;
 
-
+	al_destroy_bitmap(ball);
 	al_destroy_bitmap(square);
 	al_destroy_bitmap(square2);
 	al_destroy_bitmap(bouncer);
+	al_destroy_font(font);
 	al_destroy_timer(timer);
 	al_destroy_display(display);
 	al_destroy_event_queue(event_queue);
 
 	return 0;
+	cout << "flag11" << endl;
 } //main
 
-int collision(int b1x, int b1y, int b1w, int b1h, int b2x, int b2y, int b2w, int b2h) {
-		//if ball is to left of paddle
-	if ((b1x>b2x+b2w==1) ||
+int collision(int b1x, int b1y, int b1w, int b1h, int b2x, int b2y, int b2w, int b2h, int bouncer_dx, int bouncer_dy) {
+	//if ball is to left of paddle
+	if ((b1x + b1w < b2x) ||
 		//if ball is above paddle
-		(b1y>b2y+b2h==1) ||
+		(b1y + b1h < b2y) ||
 		//if ball is below paddle
-		(b1y>b2y+b1w==1) ||
+		(b1y > b2y + b2h) ||
 		//if ball is to right of paddle
-		(b1x>b2x+b2h==1))
+		(b1x > b2x + b2w)) {
 		return 0;
+		bouncer_dx = bouncer_dx * 3;
+		bouncer_dy = bouncer_dy * 3;
+	}
 	else
 		return 1;
 }
-
